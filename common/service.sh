dc273e852f94f0168845b0b19f5a463de76863b9
therealahrion <ahrionmgallegos@gmail.com>
2017-03-26 08:59:09 -0600

    fix service.sh

--- common/service.sh
+++ common/service.sh
@@ -6,5 +6,5 @@ MODDIR=${0%/*}
 # This script will be executed in late_start service mode
 # More info in the main Magisk thread
 
-/data/magisk/sepolicy-inject --live -s mediaserver -t mediaserver_tmpfs -c file -p read,write,execute
-/data/magisk/sepolicy-inject --live -s audioserver -t audioserver_tmpfs -c file -p read,write,execute
+/data/magisk/sepolicy-inject --live "allow mediaserver mediaserver_tmpfs file { read write execute }" \
+"allow audioserver audioserver_tmpfs file { read write execute }" 
