f54561a806d640aa725e5f42badb9cc74e09464b
Zackptg5 <Zackptg5@comcast.net>
2017-08-19 11:34:29 -0400

    Bug fixes, switch to modular setup

---
+++ common/unity-patches-remove.sh
@@ -0,0 +1,8 @@
+# v DO NOT MODIFY v
+# See instructions file for predefined variables
+# Add patches (such as audio config) here (starting at line 9)
+# NOTE: Destination variable must have '$UNITYPATCH' in front of it
+# Patch Ex: sed -i '/v4a_standard_fx {/,/}/d' $UNITYPATCH$CONFIG_FILE
+#
+#
+# ^ DO NOT MODIFY ^
