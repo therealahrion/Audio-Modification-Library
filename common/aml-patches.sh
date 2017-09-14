072022ce056bba1b4ad04d838aa9d3b4301fe5c8
Zackptg5 <zackptg5@comcast.net>
2017-09-14 00:14:53 -0400

    Add output_session patching, add uninstall for patches

--- common/aml-patches.sh
+++ common/aml-patches.sh
@@ -14,3 +14,12 @@ for CFG in $CONFIG_FILE $HTC_CONFIG_FILE $OTHER_V_FILE $OFFLOAD_CONFIG $V_CONFIG
     sed -i 's/^libraries {/libraries {\n  proxy {#\n    path \/system\/lib\/soundfx\/libeffectproxy.so\n  }/g' $AMLPATH$CFG
   fi
 done
+for CFG in $CONFIG_FILE $V_CONFIG_FILE do
+  if [ -f $CFG ] && [ ! "$(grep '#output_session_processing' $CFG)" ] && [ "$(grep '^output_session_processing' $CFG)" ]; then
+	sed -i ':a;N;$!ba;s/}\n/}@/g' $AMLPATH$CFG
+    sed -i '/^output_session_processing {/,/}.*}.*}/ s/^/#/' $AMLPATH$CFG
+    sed -i 's/#\(.*\)}@\(.*\)}@\(.*\)}@/#\1}@\2#}@\3#}@#/' $AMLPATH$CFG
+    sed -i -e ':a;N;$!ba;s/}@/}\n/g' -e 's/}#@/}#\
+	/' $AMLPATH$CFG
+  fi
+done
