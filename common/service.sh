01b07605e7992239cfafa044848b7fda585766c0
Zackptg5 <zackptg5@comcast.net>
2017-11-01 17:06:51 -0400

    Update version numbers (nowhere near release lol)

--- common/service.sh
+++ common/service.sh
@@ -1,4 +1,4 @@
 # v DO NOT MODIFY v
-# Only predefined variables for this script are: $ROOT, $SYS, $VEN, $MODID, $AMLPATH, $MAGISK, $XML_PRFX, $EXT, $SH, and $SEINJECT
+# Only predefined variables for this script are: $ROOT, $SYS, $VEN, $MODID, $AMLPATH, $MAGISK, $XML_PRFX, $SH, and $SEINJECT
 # Add any script logic you want run here (late service start in magisk and normal boot time scripts otherwise)
 # ^ DO NOT MODIFY ^
